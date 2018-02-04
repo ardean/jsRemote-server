0f0e81e3f81587ea17dc8869e4ca348d4588ffeb
ardean <aron.stifani@gmail.com>
2018-02-04 23:09:42 +0100

    refactor server, added bonjour service

--- demo/index.ts
+++ demo/index.ts
@@ -1,16 +1,17 @@
 import { Server } from "../src";
 import { log } from "util";
 
-const server = new Server();
+(async () => {
+  const server = new Server();
+  server.on("clientConnect", socket => {
+    let address = socket.request.connection.remoteAddress.replace(/^.*:/, '');
+    if (address.toString() === "1") address = "127.0.0.1";
 
-server.on("clientConnect", socket => {
-  let address = socket.request.connection.remoteAddress.replace(/^.*:/, '');
-  if (address.toString() === "1") address = "127.0.0.1";
+    log("new client connected: " + address);
+    socket.once("disconnect", () => log("client disconnected: " + address));
+  });
 
-  log("new client connected: " + address);
-  socket.once("disconnect", () => log("client disconnected: " + address));
-});
+  await server.start();
 
-server.listen(() => {
   log(`jsRemote is running on port ${server.port}`);
-});
+})();
--- src/Server.ts
+++ src/Server.ts
@@ -1,18 +1,25 @@
 import { EventEmitter } from "events";
 import * as path from "path";
 import * as express from "express";
+import * as createBonjour from "bonjour";
 import * as createSocketIOServer from "socket.io";
 import Screen from "./Screen";
 import Mouse from "./Mouse";
 import Keyboard from "./Keyboard";
 import { Server } from "net";
 
-export default class RemoteJSServer extends EventEmitter {
+const bonjour = createBonjour();
+const bonjourNamespace = "io-github-ardean-jsremote";
+
+export default class JSRemoteServer extends EventEmitter {
+  bonjour: boolean;
+  bonjourService: any;
+  refreshIntervalTimer: NodeJS.Timer = null;
+  status: string = "Stopped";
   webroot: string;
   io: SocketIO.Namespace;
   socket: Server;
   app: express.Application;
-  started: boolean = false;
   screen: Screen;
   mouse: Mouse;
   keyboard: Keyboard;
@@ -30,51 +37,91 @@ export default class RemoteJSServer extends EventEmitter {
     this.port = options.port || 4444;
     this.webroot = options.webroot;
     this.refreshInterval = options.refreshInterval || 10 * 1000;
+    this.bonjour = typeof options.bonjour === "boolean" ? options.bonjour : true;
   }
 
-  listen(port, callback?) {
-    if (this.started) return;
+  get running() {
+    return this.status !== "Stopped";
+  }
 
-    if (typeof port === "function") {
-      callback = port;
-      port = null;
-    }
-
-    port = this.port = port || this.port;
-    callback = callback || function () { };
+  async start() {
+    if (this.status !== "Stopped") return;
+    this.setStatus("Starting");
 
     this.app = express();
     this.app.disable("x-powered-by");
     this.app.use(express.static(this.webroot || path.join(__dirname, "../client")));
 
-    this.socket = this.app.listen(port, () => {
-      this.startRefreshInterval();
-      this.started = true;
-      this.emit("start");
-      callback();
+    const startPromise = new Promise((resolve) => {
+      this.socket = this.app.listen(this.port, () => {
+        this.startRefreshInterval();
+        this.setStatus("Started");
+        if (this.bonjour) {
+          this.bonjourService = bonjour.publish({ name: bonjourNamespace, type: "http", port: this.port });
+        }
+        this.emit("start");
+        resolve();
+      });
     });
 
     this.io = createSocketIOServer(this.socket, {
       path: "/sockets"
-    }).on("connection", (socket) => {
-      socket
-        .on("mouseMove", (movementX, movementY) => this.mouse.move(movementX, movementY))
-        .on("mouseDown", (key) => this.mouse.down(key))
-        .on("mouseUp", (key) => this.mouse.up(key))
-        .on("mouseScroll", (amount, direction) => this.mouse.scroll(amount, direction))
-        .on("keyboardDown", (key) => this.keyboard.down(key))
-        .on("keyboardUp", (key) => this.keyboard.up(key));
+    })
+      .on("connection", socket => {
+        socket
+          .on("mouseMove", (movementX, movementY) => this.mouse.move(movementX, movementY))
+          .on("mouseDown", key => this.mouse.down(key))
+          .on("mouseUp", key => this.mouse.up(key))
+          .on("mouseScroll", (amount, direction) => this.mouse.scroll(amount, direction))
+          .on("keyboardDown", key => this.keyboard.down(key))
+          .on("keyboardUp", key => this.keyboard.up(key));
 
-      this.emit("clientConnect", socket);
+        this.emit("clientConnect", socket);
+      });
+
+    return await startPromise;
+  }
+
+  async stop() {
+    if (this.status !== "Started") return;
+
+    this.setStatus("Stopping");
+    await new Promise((resolve, reject) => {
+      this.stopRefreshInterval();
+      this.socket.close(() => {
+        this.setStatus("Stopped");
+        resolve();
+      });
     });
 
-    return this.socket;
+    if (this.bonjourService) {
+      await new Promise((resolve) => {
+        this.bonjourService.stop(() => resolve());
+      });
+      this.bonjourService = null;
+    }
+  }
+
+  async restart() {
+    if (this.status !== "Started" && this.status !== "Stopped") return;
+    if (this.status === "Started") await this.stop();
+    await this.start();
   }
 
   startRefreshInterval() {
-    setInterval(() => {
+    this.refreshIntervalTimer = setInterval(() => {
       this.screen.refreshSize();
       this.emit("refresh");
     }, this.refreshInterval);
+  }
+
+  stopRefreshInterval() {
+    clearInterval(this.refreshIntervalTimer);
+    this.refreshIntervalTimer = null;
+  }
+
+  setStatus(status) {
+    this.status = status;
+    this.emit("statusChange", this.status);
   }
 }
