2d1836d2e4bf6967795ca0983ffb5ebf9d469d79
ardean <aron.stifani@gmail.com>
2018-02-19 22:34:47 +0100

    v0.3.2

--- demo/index.ts
+++ demo/index.ts
@@ -4,7 +4,7 @@ import { log } from "util";
 (async () => {
   const server = new Server();
   server.on("clientConnect", socket => {
-    let address = socket.request.connection.remoteAddress.replace(/^.*:/, '');
+    let address = socket.request.connection.remoteAddress.replace(/^.*:/, "");
     if (address.toString() === "1") address = "127.0.0.1";
 
     log("new client connected: " + address);
